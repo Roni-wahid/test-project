309544c74e08c55104631820063a2ec2e1505c7a
Roni-wahid <roniwahid555@gmail.com>
2023-01-11 14:54:10 +0600

    first commit

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/restaurantapp/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/mytestproject/testproject/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
