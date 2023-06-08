ac0b9c04d62174659e52e127c28d415acc5f2218
Roni-wahid <roniwahid555@gmail.com>
2023-06-08 19:13:57 +0600

    add date picker

--- src/navigations/Root.tsx
+++ src/navigations/Root.tsx
@@ -6,12 +6,14 @@ import SearchScreen from '../screens/search-screen';
 import SuccessScreen from '../screens/success-screen';
 import ErrorScreen from '../screens/error-screen';
 import LoaderScreen from '../screens/loader-screen';
+import DateRangePicker from '../screens/date-picker';
 
 const Stack = createNativeStackNavigator();
 
 const Root = () => {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="DateRange Picker" component={DateRangePicker} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Search" component={SearchScreen} />
       <Stack.Screen name="Success" component={SuccessScreen} />
---
+++ src/screens/date-picker/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DateRangePickerView from './View';
+
+const DateRangePicker = () => {
+  return (
+    <>
+      <DateRangePickerView />
+    </>
+  );
+};
+
+export default DateRangePicker;
