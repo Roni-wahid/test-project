86768b6b381ff495d91c1c67800e3f1e19ee3ccb
Roni-wahid <roniwahid555@gmail.com>
2023-06-11 13:12:27 +0600

    update date range picker

--- src/screens/date-picker/View.tsx
+++ src/screens/date-picker/View.tsx
@@ -1,76 +1,46 @@
-// import {View, Text} from 'react-native';
-// import React from 'react';
-
-// const DateRangePickerView = () => {
-//   return (
-//     <View>
-//       <Text>DatePickerRNView</Text>
-//     </View>
-//   );
-// };
-
-// export default DateRangePickerView;
-
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {View, Text} from 'react-native';
+import React, {useState} from 'react';
 import CalendarPicker from 'react-native-calendar-picker';
+import t from '../../styles/theme';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedStartDate: null,
-      selectedEndDate: null,
-    };
-    this.onDateChange = this.onDateChange.bind(this);
-  }
+const DateRangePicker = () => {
+  const [selectedStartDate, setSelectedStartDate] = useState('');
+  const [selectedEndDate, setSelectedEndDate] = useState('');
 
-  onDateChange(date, type) {
+  const minDate = new Date(); // Today
+  const maxDate = new Date(2025, 6, 3);
+  const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+  const endDate = selectedEndDate ? selectedEndDate.toString() : '';
+
+  const onDateChange = (date, type) => {
     if (type === 'END_DATE') {
-      this.setState({
-        selectedEndDate: date,
-      });
+      setSelectedEndDate(date);
     } else {
-      this.setState({
-        selectedStartDate: date,
-        selectedEndDate: null,
-      });
+      setSelectedStartDate(date);
+      setSelectedEndDate(null);
     }
-  }
+  };
+  return (
+    <View>
+      <CalendarPicker
+        startFromMonday={true}
+        allowRangeSelection={true}
+        minDate={minDate}
+        maxDate={maxDate}
+        // scrollable={true}
+        // horizontal={false}
+        todayBackgroundColor="#DF0039"
+        selectedDayColor="#7300e6"
+        selectedDayTextColor="#FFFFFF"
+        selectedRangeStartStyle={[t.bgError]}
+        selectedRangeStyle={[t.bgErrorLight]}
+        selectedRangeEndStyle={[t.bgError]}
+        onDateChange={onDateChange}
+      />
+      <Text>SELECTED START DATE:{startDate}</Text>
+      <Text>SELECTED END DATE:{endDate}</Text>
+    </View>
+  );
+};
 
-  render() {
-    const {selectedStartDate, selectedEndDate} = this.state;
-    const minDate = new Date(); // Today
-    const maxDate = new Date(2025, 6, 3);
-    const startDate = selectedStartDate ? selectedStartDate.toString() : '';
-    const endDate = selectedEndDate ? selectedEndDate.toString() : '';
-
-    return (
-      <View style={styles.container}>
-        <CalendarPicker
-          startFromMonday={true}
-          allowRangeSelection={true}
-          minDate={minDate}
-          maxDate={maxDate}
-          todayBackgroundColor="#f2e6ff"
-          selectedDayColor="#7300e6"
-          selectedDayTextColor="#FFFFFF"
-          onDateChange={this.onDateChange}
-        />
-
-        <View>
-          <Text>SELECTED START DATE:{startDate}</Text>
-          <Text>SELECTED END DATE:{endDate}</Text>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-    marginTop: 100,
-  },
-});
+export default DateRangePicker;
